80a1a826a040e2c6c625ad35b50940959f61ffcc
andreiizotov18 <57590487+andreiizotov18@users.noreply.github.com>
2019-12-15 12:42:05 +0200

    Add files via upload

---
+++ ghy.js
@@ -0,0 +1,84 @@
+function showMessage()
+    alert( 'привет всем пристувушгракптоу!');
+)
+
+
+
+showMessage();
+
+showMessage();
+
+showMessage()
+
+function num (a, b) {
+    return a + b;
+}
+var a = sum(2, 3);
+alert (Sum: " + s);
+
+function div (firstNum, secondNum) {
+    alert (firstNum + " * "
+        secondNum + " = " +
+        (firstNum * secondNum));
+    return firstNum * secondNum
+}
+div(1, 2);
+alert(sum(:2, div(2, 2)));
+
+function Star (count) (
+    for (var i = 0; i < count; i++) {
+        document.write(text ' *');
+    }
+        document.write(text" <br> ");
+)
+Star(count: 30);
+Star(count: 10);
+Star(count: 50);
+
+
+function vivod(num) {
+    var n = paraeInt(num);
+    if (n > 0 || n < 10) alert(n);
+    else alert("нет совпалений");
+}
+vivod(num: 15);
+
+
+function AnyLine(symb, cont) {
+    for(var i = 0; i < count; i++) {
+        document.write(symb);
+    }
+    document.write( "<br>");
+}
+AnyLine(symb: '|', count:10);
+AnyLine(symb: '++', count:3);
+AnyLine(symb: '=', count:5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function diapazon(num) {
+    var n = parseInt(num);
+    if (n > 0 || n < 10) return n;
+    else return 0;
+}
+
+
+var number = diapazon(num "11 2");
+alert(number);
+
+
+
+
+
+
